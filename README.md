ff5856ff38e67faaf27b87ab7e17d089be571d0e
DavidConstante <omileconstante@gmail.com>
2023-05-31 11:52:56 -0500

    feat: :tada: Start the project
    Configure react with tailwind

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# react-tailwind
-This is a template to start a project using react and tailwind
+# React Js + Tailwind Css🖌️!
+
+This is a template to start a new project whit a start configuration. Please use **yarn** as a package manager.
+
+    yarn
+    yarn dev --host
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen bg-slate-700'>
+      <div className='flex flex-col text-white text-center'>
+        <h1 className='text-4xl text-white font-extrabold mb-8'>React App + Tailwind Css 🖌️</h1>
+        <a href='https://github.com/DavidConstante' target='_blank' className='hover:text-slate-500' rel="noreferrer">By Célimo Constante</a>
+      </div>
+    </div>
+  )
+}
+
+export default App
